9471cfc7ae605ebcbea362446a6bbb395690f80d
mberkayk <45337705+mberkayk@users.noreply.github.com>
2021-05-24 10:21:15 +0300

    organization and app name

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QCoreApplication::setOrganizationName("mberkayk");
+    QCoreApplication::setApplicationName("Journalling-App");
+
     MainWindow w;
     w.show();
     return a.exec();
